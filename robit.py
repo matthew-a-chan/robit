b6d9c8a04976ae4662ef3244efec26bfb6412936
Matthew Chan <chan.matthew99@gmail.com>
2020-05-31 21:59:16 -0700

    Changed robit.Robit to store multiple controllers

--- robit.py
+++ robit.py
@@ -5,44 +5,55 @@ import math
 
 
 
-
-
 class Robit ():
 
-    def __init__ (self):
-        print ("looking for device")
-        self.device = odrive.find_any ()
-        print ("found device")
+    def __init__ (self, motors):
+        self.devices = dict()
+        self.motor_table = dict()
+        for serial, axis, label in motors:
+            print (f"looking for device {serial}")
+            device = odrive.find_any (serial_number = serial)
+            print ("found device")
 
-    def set_state (self, axis, state):
-        motor = self.get_motor (axis)
+            self.devices[serial] = device
+            self.motor_table[label] = (serial, axis)
+                
+
+    def set_state (self, label, state):
+        motor = self.get_motor (label)
         if state == 'IDLE':
             motor.requested_state = 1 # IDLE
-        if state == 'HOLD':
-            motor.requested_state = 8 # Lock position
+        if state == 'CONTROL':
+            motor.requested_state = 8 # Controlled
         if state == 'CALIBRATE':
             motor.requested_state = 3 # CALIBRATION
         
-    def snap (self, axis, position):
-        motor = self.get_motor (axis)
+    def snap (self, label, position):
+        motor = self.get_motor (label)
         motor.controller.pos_setpoint = position
 
-    #def go_to (self, axis, position):
-    #    motor = self.get_motor (axis)
-    #    motor.controller.move_to_pos (position)
+    def measure_resistance (self, label):
+        motor = self.get_motor (label)
+        return motor.motor.current_meas_phB
 
-    def get_motor (self, axis):
+    def get_motor (self, label):
+        # If need be, we may easily optimize this.
+        serial, axis = self.motor_table[label]
+        device = self.devices[serial]
         if axis == 0:
-            return self.device.axis0
+            return device.axis0
         else:
-            return self.device.axis1
+            return device.axis1
         
-    def test_sin (self, revolutions, amplitude):
-        iterations = 10000
+    def test_sin (self, label, revolutions, amplitude):
+        self.set_state (label, 'CONTROL')
+        iterations = 1000
         for i in range (iterations):
-            self.snap (0, math.sin(revolutions * 6.282 * i / iterations) * amplitude)
-            time.sleep (50 / iterations)
+            self.snap (label, math.sin(revolutions * 6.282 * i / iterations) * amplitude)
+            time.sleep (5.0 / iterations)
+        self.set_state (label, 'IDLE')
 
+        
 
 
 
