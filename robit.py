f8e5ccdbf8c02eeb7e0f69d8a532f2152146df75
Matthew Chan <chan.matthew99@gmail.com>
2020-06-06 22:59:03 -0700

    also updated robit

--- robit.py
+++ robit.py
@@ -1,23 +1,31 @@
 import odrive
 import time
 import math
-
+import tinyik
 
 
 
 class Robit ():
 
-    def __init__ (self, motors):
+    def __init__ (self, controllers):
         self.devices = dict()
         self.motor_table = dict()
-        for serial, axis, label in motors:
+        for serial, label1, label2 in controllers:
             print (f"looking for device {serial}")
             device = odrive.find_any (serial_number = serial)
             print ("found device")
 
             self.devices[serial] = device
-            self.motor_table[label] = (serial, axis)
-                
+            self.motor_table[label1] = (serial, 0)
+            self.motor_table[label2] = (serial, 1)
+        
+        self.legs = dict()
+
+        self.legs['Front_Left'] =  tinyik.Actuator(['x', [0., -20., .0], 'y', [0., 0., -116.], 'y', [0., 10., -116.]])
+        self.legs['Front_Right'] = tinyik.Actuator(['x', [0., 20., .0], 'y', [0., 0., -116.], 'y', [0., -10., -116.]])
+        self.legs['Back_Left'] =   tinyik.Actuator(['x', [0., -20., .0], 'y', [0., 0., -116.], 'y', [0., 10., -116.]])
+        self.legs['Back_Right'] =  tinyik.Actuator(['x', [0., 20., .0], 'y', [0., 0., -116.], 'y', [0., -10., -116.]])
+
 
     def set_state (self, label, state):
         motor = self.get_motor (label)
@@ -27,8 +35,13 @@ class Robit ():
             motor.requested_state = 8 # Controlled
         if state == 'CALIBRATE':
             motor.requested_state = 3 # CALIBRATION
-        
-    def snap (self, label, position):
+    
+    def snap (self, labels, positions):
+        SCALE = 9*7200/6.14159 # ~10551.1
+        for label, position in zip (labels, positions):
+            self.snap_units (label, SCALE * position)
+    
+    def snap_units (self, label, position):
         motor = self.get_motor (label)
         motor.controller.pos_setpoint = position
 
@@ -36,6 +49,15 @@ class Robit ():
         motor = self.get_motor (label)
         return motor.motor.current_meas_phB
 
+    def reset (self, labels, positions):
+        for label, position in zip (labels, positions):
+            self.set_state (label, 'IDLE')
+            motor = self.get_motor (label)
+            motor.encoder.set_linear_count(position)
+            self.snap_units (label, position)
+            self.set_state (label, 'CONTROL')
+
+    
     def get_motor (self, label):
         # If need be, we may easily optimize this.
         serial, axis = self.motor_table[label]
@@ -44,14 +66,42 @@ class Robit ():
             return device.axis0
         else:
             return device.axis1
+    
+    def place (self, positions):
+        print ("This is a really _really_ _REALLY_ bad idea to run this function right now.")
+        return
+        if 'Front_Left' in positions:
+            self.legs['Front_Left'].ee = positions['Front_Left']
+            self.snap (['FL_1', 'FL_2', 'FL_3'], self.legs['Front_Left'].angles)
+        if 'Front_Right' in positions:
+            self.legs['Front_Right'].ee = positions['Front_Right']
+            self.snap (['FR_1', 'FR_2', 'FR_3'], self.legs['Front_Right'].angles)
+        if 'Back_Left' in positions:
+            self.legs['Back_Left'].ee = positions['Back_Left']
+            self.snap (['BL_1', 'BL_2', 'BL_3'], self.legs['Back_Left'].angles)
+        if 'Back_Right' in positions:
+            self.legs['Back_Right'].ee = positions['Back_Right']
+            self.snap (['BR_1', 'BR_2', 'BR_3'], self.legs['Back_Right'].angles)
         
-    def test_sin (self, label, revolutions, amplitude):
-        self.set_state (label, 'CONTROL')
+
+
+
+
+
+
+
+
+
+
+
+        
+    def test_sin (self, labels, revolutions, amplitude):
+        #self.set_state (labels, 'CONTROL')
         iterations = 1000
         for i in range (iterations):
-            self.snap (label, math.sin(revolutions * 6.282 * i / iterations) * amplitude)
+            self.snap (labels, [math.sin(revolutions * 6.282 * i / iterations) * amplitude] * len(labels))
             time.sleep (5.0 / iterations)
-        self.set_state (label, 'IDLE')
+        #self.set_state (label, 'IDLE')
 
         
 
@@ -64,9 +114,3 @@ class Robit ():
 
 
 
-
-
-
-
-if __name__ == '__main__':
-    pass
